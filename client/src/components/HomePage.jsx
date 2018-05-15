8fb9464174f8ee8db8082882b832dd5e1ce290f6
sriramram1995 <sriramram1995@gmail.com>
2018-05-15 22:54:47 +0530

    reactauth

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, CardTitle } from 'material-ui/Card';
+
+
+const HomePage = () => (
+  <Card className="container">
+    <CardTitle title="Home page" subtitle="Please login to enter the school finder app." />
+  </Card>
+);
+
+export default HomePage;
